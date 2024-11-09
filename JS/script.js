2b7fd2bcc6a33e7d1fdfe602e41b3cf7a3e4a594
ManuFlorenca! <manuelaflorenca22@gmail.com>
2024-11-09 14:58:40 -0300

    alterando login

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,17 @@
-const wrapper = document.querySelector(`.wrapper`);
-const loginLink = document.querySelector(`.login-link`);
-const loginRegister = document.querySelector(`.register-link`);
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const user = document.querySelector('.user');
 
-registerLink.addEventListener('click',()=>{
-    wrapper.classList.add('active')
+registerLink.addEventListener('click', () => {
+    wrapper.classList.add('active');
 });
 
-loginLink.addEventListener('click',()=>{
-    wrapper.classList.remove('active')
+loginLink.addEventListener('click', () => {
+    wrapper.classList.remove('active');
 });
+
+user.addEventListener('click', () => {
+    wrapper.classList.remove('active');
+});
+
