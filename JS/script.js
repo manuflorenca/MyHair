cf530b02cc41f1dca7b68fb9c38f50bbd954f8fa
ManuFlorenca! <manuelaflorenca22@gmail.com>
2024-11-09 14:43:15 -0300

    Primeira mudança

--- JS/script.js
+++ JS/script.js
@@ -1,28 +1,11 @@
-const t_field_nome=document.querySelector("#nome");
-const t_field_senha=document.querySelector("#senha");
-const form_login=document.querySelector("#form_login")
+const wrapper = document.querySelector(`.wrapper`);
+const loginLink = document.querySelector(`.login-link`);
+const loginRegister = document.querySelector(`.register-link`);
 
+registerLink.addEventListener('click',()=>{
+    wrapper.classList.add('active')
+});
 
-const validName = 'manu';
-const validPassword = '123';
-
-function entrarSistema(e){
-    // e.preventDefault();
- 
-    if (t_field_nome.value === validName && t_field_senha.value === validPassword) {
-        // Login bem-sucedido, armazena no localStorage
-        localStorage.setItem('isLoggedIn', 'true');
-        window.location.href = 'login.html';
-    } else {
-        // Exibe a mensagem de erro
-        document.getElementById('error-message').textContent = 'Nome ou senha incorretos';
-    }
-   
-};
- 
-form_login.addEventListener("submit",entrarSistema);
- 
-if (localStorage.getItem('isLoggedIn')) {
-    window.location.href = 'index.html';
-}
- 
+loginLink.addEventListener('click',()=>{
+    wrapper.classList.remove('active')
+});
