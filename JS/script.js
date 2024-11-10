e52b2c33daec5bfc35a0b95ad243a3545c7d3e3c
ManuFlorenca! <manuelaflorenca22@gmail.com>
2024-11-09 23:43:44 -0300

    Ultima atualização dia 9/11

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,25 @@
 const wrapper = document.querySelector('.wrapper');
+const btnPopup = document.querySelector('#btnPopup');
+const iconClose = document.querySelector('.icon-close');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-const user = document.querySelector('.user');
+
+
+btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+});
+
+
+iconClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+});
+
 
 registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
 });
 
+
 loginLink.addEventListener('click', () => {
     wrapper.classList.remove('active');
 });
-
-user.addEventListener('click', () => {
-    wrapper.classList.remove('active');
-});
-
