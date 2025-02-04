54d7db47f361563f894e6b8fbaa915b603e4be6d
João Matos <joaovitormatos34@outlook.com>
2025-02-04 08:42:31 -0300

    login otimizado

--- JS/login.js
+++ JS/login.js
@@ -1,22 +1,30 @@
 const API_URL = 'http://127.0.0.1:5000/usuario';
- 
- 
-const nome=document.querySelector("#nome");
-const senha=document.querySelector("#senha");
-const userForm=document.querySelector("#formLogin");
- 
-  userForm.addEventListener('submit', async (e) => {
+
+const nome = document.querySelector("#nome");
+const senha = document.querySelector("#senha");
+const userForm = document.querySelector("#formLogin");
+
+userForm.addEventListener('submit', async (e) => {
     e.preventDefault();
-    const response = await fetch(API_URL);
-    // Trás os dados via API
-    const users = await response.json();
-    const user = users.find(user => user.Nome === nome.value && user.Senha === senha.value);
-    if (user) {
-        // Se o usuário for encontrado, cria a sessão e redireciona para o menu
-        sessionStorage.setItem('usuario', user.Nome);  // Armazena na sessão local do navegador (sessionStorage)
-        window.location.href = "agenda.html";  // Redireciona para a página menu.html
-    } else {
-        alert("Usuário ou senha inválidos!");
+    
+    try {
+        const response = await fetch(API_URL);
+        
+        if (!response.ok) {
+            throw new Error('Erro ao acessar a API');
+        }
+
+        const users = await response.json();
+        const user = users.find(user => user.Nome === nome.value && user.Senha === senha.value);
+        
+        if (user) {
+            sessionStorage.setItem('usuario', user.Nome);
+            window.location.href = "agenda.html";
+        } else {
+            alert("Usuário ou senha inválidos!");
+        }
+    } catch (error) {
+        console.error('Erro:', error);
+        alert("Ocorreu um erro ao tentar fazer login. Tente novamente mais tarde.");
     }
- 
-  })
+});
